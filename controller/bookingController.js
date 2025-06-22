129327ec6ee02565ae3c7b00f4f0d89fff0f706e
ekogift <ekogift2000@gmail.com>
2025-06-22 09:35:30 +0100

    webhook enhanced

--- controller/bookingController.js
+++ controller/bookingController.js
@@ -29,7 +29,7 @@ exports.getCheckoutSession = async (req, res, next) => {
     mode: 'payment',
     // success_url: `${req.protocol}://${req.get('host')}/?user=${req.user.id}&tour=${tour.id}&price=${tour.price}`,
 
-    success_url: `${req.protocol}://${req.get('host')}/webhook-checout`,
+    success_url: `${req.protocol}://${req.get('host')}/my-tours `,
     cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
     customer_email: req.user.email,
     client_reference_id: req.params.tourId,
